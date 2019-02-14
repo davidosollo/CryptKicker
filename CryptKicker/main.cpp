c0371884cf9d78802268fb9abd0353d2120aee84
David Osollo <david_osollo@yahoo.com>
2019-02-14 10:24:11 -0600

    Initial Commit

---
+++ CryptKicker/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CryptKicker
+//
+//  Created by David Osollo on 2/14/19.
+//  Copyright © 2019 David Osollo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
